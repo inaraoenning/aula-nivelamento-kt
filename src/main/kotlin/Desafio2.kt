364634f8d0809ba562cb52968ffe5b749b420e6a
inara <inaraoenning@gmail.com>
2026-02-26 12:39:12 -0300

    Finalizando Desafio 2

--- src/main/kotlin/Desafio2.kt
+++ src/main/kotlin/Desafio2.kt
@@ -2,29 +2,41 @@ fun main() {
     // Ler 2 numeros inteiros
     // Console com opcoes + - / *
 
-    println("Digite o primeiro numero:")
-    var num1 = readln().toInt()
+    while (true) {
 
-    println("Digite o segundo numero:")
-    var num2 = readln().toInt()
+        println(
+            """=======================
+Selecione uma operação
+1-Soma
+2-Subtração
+3-Multiplicação
+4-Divisão
+5-Sair
+                """
 
-    println(
-        """Selecione uma operação
-                1-Soma
-                2-Subtração
-                3-Multiplicação
-                4-Divisão
-                5-Sair"""
+        )
+        var opcao: Int = readln().toInt()
 
-    )
-    var operacao: Int = readln().toInt()
+        if (opcao !in 1..4) return
 
-    when (operacao) {
-        1 -> println("num1+ num2 = ${num1 + num2}")
-        2 -> println("num1 - num2 = ${num1 - num2}")
-        3 -> println("num1 * num2 = ${num1 * num2}")
-        4 -> println("num1 / num2 = ${num1 / num2}")
-        else -> return
+        print("Digite o primeiro numero:")
+        var num1 = readln().toInt()
 
+        print("Digite o segundo numero:")
+        var num2 = readln().toInt()
+
+        when (opcao) {
+            1 -> println("$num1 + $num2 = ${num1 + num2}")
+            2 -> println(" $num1 - $num2 = ${num1 - num2}")
+            3 -> println(" $num1 * $num2 = ${num1 * num2}")
+            4 -> {
+                if (num2 == 0) {
+                    println("⚠️ Não é possivel dividir por zero.")
+                } else println(" $num1 / $num2 = ${num1 / num2}")
+            }
+
+            else -> return
+
+        }
     }
 }
