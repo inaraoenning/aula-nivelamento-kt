577a8111997d40c3c9c404afcd636c06204bcee4
inara <inaraoenning@gmail.com>
2026-02-26 13:01:20 -0300

    Add descrição desafio3

--- src/main/kotlin/Desafio3.kt
+++ src/main/kotlin/Desafio3.kt
@@ -1,43 +1,13 @@
 fun main() {
-    // Ler 2 numeros inteiros
-    // Console com opcoes + - / *
 
-    while (true) {
+    // menu Compra Venda Estoque Financeiro
 
-        println(
-            """
-=====================
-Escolha uma operação:
-1- Somar
-2- Subtração
-3- Multiplicação
-4- Divisão
-5- Sair
-        """
-        )
-        var opcao: Int = readln().toInt();
+    // Compra: Armazenar Nome QTD Valor (Descontar do financeiro) (To uppercase)
+    // Venda: Removar Nome QTD Valor (Adicionar ao financeiro +5%)
+    // Estoque: Mostra itens em estoque
+    // Financeiro: Inicia com 100 / mostra saldo atual do financeiro
 
-        if (opcao !in 1..4) {
-            return
-        }
 
-        print("Insira o primeiro numero:")
-        var num1: Int = readln().toInt();
-
-        print("Insira o segundo numero:")
-        var num2: Int = readln().toInt();
-
-        when (opcao) {
-            1 -> println("$num1 + $num2 = ${num1 + num2}")
-            2 -> println("$num1 - $num2 = ${num1 - num2}")
-            3 -> println("$num1 * $num2 = ${num1 * num2}")
-            4 -> {
-                if (num2 == 0) {
-                    println("⚠️ Não é possivel dividir por zero.")
-                } else println("$num1 / $num2 = ${num1 / num2}")
-            }
-        }
-    }
 
 
 }
