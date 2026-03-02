172a90c0b57ab8365fde614a2b1e66b2a04e6bed
Feyre <inaraoenning@gmail.com>
2026-03-02 20:04:15 -0300

    reforcoDesafio.kt

--- src/main/kotlin/ReforcoDesafio.kt
+++ src/main/kotlin/ReforcoDesafio.kt
@@ -13,11 +13,36 @@ fun main (){
     * 0 - Sair
     */
 
+    // Menu Biblioteca
+    println("========================")
+    print(
+        """
+Selecione uma operação
+1- Cadastrar
+2- Emprestar
+3- Devolver 
+4- Ver catálogo
+5- Ver usuários e empréstimos
+6- Financeiro (multas arrecadadas)
+0- Sair
+    """.trimIndent()
+    )
+
 }
 
 
 data class Livro(val titulo: String, val autor:String, var disponivel: Boolean) {
 
+}
+
+class Biblioteca {
+    private var saldoFinanceiro: Double = 0.0
+    private val estoque: MutableList<Livro> = mutableListOf()
+
+    //1- Cadastrar Livro ou Revista
+    fun cadastrarLivro(titulo:String, autorLivro: String, disponivel: Boolean){
+
+    }
 }
 
 
