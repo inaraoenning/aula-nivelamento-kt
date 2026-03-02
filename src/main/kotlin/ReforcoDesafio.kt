a6c134291f82563b77424cb5941985bbc9b99fdc
Feyre <inaraoenning@gmail.com>
2026-03-02 19:20:09 -0300

    Add: ReforcoDesafio.kt

--- src/main/kotlin/ReforcoDesafio.kt
+++ src/main/kotlin/ReforcoDesafio.kt
@@ -1,20 +1,24 @@
 
 fun main (){
 
-//    1 - Cadastrar item (Livro ou Revista)
-//    2 - Emprestar item
-//            3 - Devolver item
-//            4 - Ver catálogo
-//            5 - Ver usuários e empréstimos
-//            6 - Financeiro (multas arrecadadas)
-//    0 - Sair
+
+    /*
+    *
+    * 1 - Cadastrar item (Livro ou Revista)
+    * 2 - Emprestar item
+            3 - Devolver item
+            4 - Ver catálogo
+            5 - Ver usuários e empréstimos
+            6 - Financeiro (multas arrecadadas)
+    * 0 - Sair
+    */
 
 }
 
 
-sealed class Itembiblioteca (
+data class Livro(val titulo: String, val autor:String, var disponivel: Boolean) {
 
-)
+}
 
 
 
